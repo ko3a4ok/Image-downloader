f31c3f3a62b4ff59abb11ae5b08a276288d71764
roma <ko3a4ok.ua@gmail.com>
2014-12-05 00:54:33 +0200

    add batch list update

--- app/src/main/java/ua/ko3a4ok/ololo/MyService.java
+++ app/src/main/java/ua/ko3a4ok/ololo/MyService.java
@@ -1,6 +1,7 @@
 package ua.ko3a4ok.ololo;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,17 +13,22 @@ import java.lang.ref.Reference;
 import java.lang.ref.SoftReference;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MyService extends Service {
     public static final String LINK = "link";
+    private static final long MIN_REFRESH_INTERVAL = 300;
+    private static final Intent UPDATE_UI_INTENT = new Intent(MyActivity.REFRESH_DATA);
 
     private static final int W = 320;
     private static final int H = 480;
 
     private boolean stopped;
 
+    private static volatile boolean emptyBatch = true;
     private MyApplication application;
     private ExecutorService poolExecutor;
 
@@ -87,7 +93,7 @@ public class MyService extends Service {
                         total += count;
                         int percentage = 100 * total / len;
                         ih.setPercentage(percentage);
-                        updateUi();
+                        updateUi(MyService.this);
                         if (stopped) {
                             failed();
                             return;
@@ -97,7 +103,7 @@ public class MyService extends Service {
                 }
                 ih.setPercentage(100);
                 saveImage(ih.getRawData());
-                updateUi();
+                updateUi(MyService.this);
             } catch (OutOfMemoryError e) {
                 e.printStackTrace();
                 failed();
@@ -108,13 +114,10 @@ public class MyService extends Service {
             }
         }
 
-        private void updateUi() {
-            LocalBroadcastManager.getInstance(MyService.this).sendBroadcast(new Intent(MyActivity.REFRESH_DATA));
-        }
 
         private void failed() {
             ih.setFailed(true);
-            updateUi();
+            updateUi(MyService.this);
         }
 
         private void saveImage(byte[] img) {
@@ -130,6 +133,18 @@ public class MyService extends Service {
             String uri = Utils.saveImage(MyService.this, result);
             ih.setUri(uri);
         }
+    }
+
+    private static synchronized void updateUi(final Context ctx) {
+        if (!emptyBatch) return;
+        emptyBatch = false;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                emptyBatch = true;
+                LocalBroadcastManager.getInstance(ctx).sendBroadcast(UPDATE_UI_INTENT);
+            }
+        }, MIN_REFRESH_INTERVAL);
     }
 
     class ImageBuffer {
